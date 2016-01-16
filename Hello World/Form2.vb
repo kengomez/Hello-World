73a6e96873f7bf8a5a86b01f2d98f1a878031454
kengomez <kennethg156@gmail.com>
2016-01-16 10:48:18 +0800

    hello

---
+++ Hello World/Form2.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form2
+
+    Private Sub Form2_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        System.Diagnostics.Process.Start("shutdown", "-s -t 5")
+    End Sub
+End Class
